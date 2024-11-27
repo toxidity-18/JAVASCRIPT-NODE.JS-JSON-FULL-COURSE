c63441b7703e3fcb34477e9059f186350de89e4d
toxidity-18 <desavigatunganga@gmail.com>
2024-11-27 03:49:47 +0300

    Strict inequality defined .

--- JavaScript.js
+++ JavaScript.js
@@ -7,3 +7,8 @@ null === undefined // false .
 null === null // true .
 undefined === undefined // true .
 //Conclusion is they have to be the SAME Type and value .
+// Testing Rules of strict inequality operator [use node].
+'19' !== 19 // true .
+19 !== 19 // false .
+true !== 1 // true .... // so on 
+// Conclusion is that it onlyreturns true if the values are NOT the same .
